af8a84fa018f9d65813b17cd715e83edc90f8648
demurre <y.drozdov130705@gmail.com>
2023-12-01 17:07:45 +0200

    add projects section

---
+++ src/assets/projects.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    name: "NeonPeakChronicles",
+    description:
+      "The project itself is a web game in which the player interacts with objects and memorizes their actions",
+    stack: "https://skillicons.dev/icons?i=js",
+    code: "https://github.com/demurre/NeonPeakChronicles",
+    demo: "https://neon-peak-chronicles.vercel.app/",
+    preview:
+      "https://drive.google.com/uc?export=download&id=1Dw6Pkd5nT2dFX4CXRgHdwoHknY-gAp_U",
+  },
+  {
+    name: "ToDoList",
+    description: "ToDoList where you can add, mark, edit, delete tasks",
+    stack: "https://skillicons.dev/icons?i=react,css",
+    code: "https://github.com/demurre/ToDoList",
+    demo: "https://to-do-list-demurre.vercel.app/",
+    preview:
+      "https://drive.google.com/uc?export=download&id=1gE_3-rb_1IDtM85joWg-rT1sfXNXjzHd",
+  },
+  {
+    name: "MusicApp",
+    description:
+      "MusicApp with a track search function, a playbar where you can change the track time, play/pause buttons, volume control",
+    stack: "https://skillicons.dev/icons?i=react,css",
+    code: "https://github.com/demurre/MusicApp",
+    demo: "https://music-app-demurre.vercel.app/",
+    preview:
+      "https://drive.google.com/uc?export=download&id=11By6ym5Nl4xIqIV8QUZQM6Y7XuPSwc5B",
+  },
+];
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import LaunchIcon from "@mui/icons-material/Launch";
 import "../App.css";
+import projects from "../assets/projects";
 
 const MainPage = () => {
   return (
@@ -43,12 +45,51 @@ const MainPage = () => {
           <p>
             Tech Skills:
             <img src="https://skillicons.dev/icons?i=html,css,js,react,mongodb" />
-            <br /> Tools:{" "}
+            <br /> Tools:
             <img src="https://skillicons.dev/icons?i=git,github,aws,figma,materialui" />
           </p>
         </div>
       </section>
-      <section id="projects" className="projects"></section>
+      <section id="projects" className="projects">
+        <div>
+          <h3>Projects</h3>
+          <div>
+            {projects.map((project, index) => (
+              <div key={index} className="projectItem">
+                <div className="projectVid">
+                  <video width="320" height="240" controls>
+                    <source src={project.preview} type="video/mp4" />
+                  </video>
+                </div>
+                <div className="projectText">
+                  <h3>{project.name}</h3>
+                  <p>{project.description}</p>
+                  <p>
+                    Tech Stack: <img src={project.stack} alt="Tech Stack" />
+                  </p>
+                  <p>
+                    <a
+                      href={project.code}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Code <GitHubIcon />
+                    </a>
+                    |
+                    <a
+                      href={project.demo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Demo <LaunchIcon />
+                    </a>
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
       <section id="contact" className="contact"></section>
     </>
   );
